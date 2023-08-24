8fa56d550f400c61d4ba4328c2a0c40251753ba5
jeyhawkes <142214413+jeyhawkes@users.noreply.github.com>
2023-08-24 11:10:21 +0100

    Update table_create.sql

--- setup/table_create.sql
+++ setup/table_create.sql
@@ -1,5 +1,5 @@
 
--- structure for table cushion.customer
+-- structure for table cushon.customer
 CREATE TABLE `customer` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT 'table row',
   `name` varchar(50) NOT NULL DEFAULT '' COMMENT 'customer name',
@@ -7,12 +7,12 @@ CREATE TABLE `customer` (
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='List of customers';
 
--- data for table cushion.customer: ~1 rows (approximately)
+-- data for table cushon.customer: ~1 rows (approximately)
 INSERT INTO `customer` (`name`) VALUES
 	('Josh');
 
 
--- structure for table cushion.customer_investments
+-- structure for table cushon.customer_investments
 CREATE TABLE `customer_investments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'table row id',
   `investment_type_id` tinyint(3) unsigned NOT NULL,
@@ -23,9 +23,9 @@ CREATE TABLE `customer_investments` (
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- data for table cushion.customer_investments: ~0 rows (approximately)
+-- data for table cushon.customer_investments: ~0 rows (approximately)
 
--- structure for table cushion.investment_types
+-- structure for table cushon.investment_types
 CREATE TABLE `investment_types` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT 'row id',
   `name` varchar(255) NOT NULL DEFAULT '',
@@ -33,7 +33,7 @@ CREATE TABLE `investment_types` (
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='List of customers';
 
--- data for table cushion.investment_types: ~2 rows (approximately)
+-- data for table cushon.investment_types: ~2 rows (approximately)
 INSERT INTO `investment_types` (`name`) VALUES
 	('Cushon Equities Fund'),
 	('Cushon Fixed income Fund');
